87bfa79b5bf30e1b6bb889e415616967d1eb7772
Morpheus453 <53342625+Morpheus453@users.noreply.github.com>
2022-09-19 20:53:39 -0400

    Add files via upload

---
+++ Shared/packetMaps.js
@@ -0,0 +1,7 @@
+export const packetMaps = {
+    "REQUEST_JOIN": 0,
+    "JOIN_RESPONSE": 1,
+
+    0: "REQUEST_JOIN",
+    1: "JOIN_RESPONSE",
+}
---
+++ Shared/packets.js
@@ -0,0 +1,3 @@
+export const packets = {
+    0: { name: "" },
+}
