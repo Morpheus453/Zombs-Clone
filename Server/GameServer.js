898e5c1b5020f92862b2b23798fc6ef7fa1ab7af
Morpheus453 <53342625+Morpheus453@users.noreply.github.com>
2022-09-20 09:33:55 -0400

    Update GameServer.js

--- Server/GameServer.js
+++ Server/GameServer.js
@@ -1,10 +1,30 @@
+const { Client } = require("./Network/Client.js");
+const { Codec } = require("../Shared/codec.js");
+
+const codec = new Codec();
+
 class GameServer {
     constructor () {
         this.clients = [];
     }
 
+    broadcast (msg) {
+        // Encode the message before sending it that way it doesn't have to encode the same
+        // Message over and over again 
+
+        msg = codec.encode(msg);
+
+        for (let i = 0; i < this.clients.length; i++) {
+            this.clients[i].websocket.send(msg);
+        }
+    }
+
     addClient (socket) {
-        this.clients.push()
+        this.clients.push(new Client(this, socket));
+    }
+
+    addEntity (eid, client) {
+        // TODO
     }
 }
 
