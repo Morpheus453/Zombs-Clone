4e3f8d09dcde7339ea57c04eb8cad3e02b1d6185
Morpheus453 <53342625+Morpheus453@users.noreply.github.com>
2022-09-21 12:17:50 -0400

    Update Circle.js

--- Server/World/Physics/Circle.js
+++ Server/World/Physics/Circle.js
@@ -1,19 +1,12 @@
 const Vector = require("../../../Shared/Vector.js");
+const Entity = require("./Entity.js");
 
-module.exports = class Circle {
-    constructor (position, radius) {
+module.exports = class Circle extends Entity {
+    constructor (label, id, world, radius) {
+        super(label, id, { x: Math.floor(Math.random() * world.width), y: Math.floor(Math.random() * world.height) })
+
+        // Label is for differentiationg between player, tree, stone, etc.
         // Position should be a vector as shown in the shared folder
-
-        this.position = position;
         this.radius = radius;
-
-        // Could just do { x: 0, y: 0 } but using Vector.create helps some with clarity
-
-        this.acceleration = Vector.create();
-        this.velocity = Vector.create();
-    }
-
-    update (dt) {
-        // TODO
     }
 }
