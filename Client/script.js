f3ec1d28bc782f517df989ac2cfe0222c0b6451d
Morpheus453 <53342625+Morpheus453@users.noreply.github.com>
2022-09-20 22:16:49 -0400

    Update script.js

--- Client/script.js
+++ Client/script.js
@@ -1,4 +1,26 @@
-import * as Renderer from "./renderer.js";
-import * as Codec from "../Shared/codec.js";
+import { Renderer } from "./renderer.js";
+import Codec from "../Shared/codec.js";
 
-console.log("Hello world");
+// In the future this will be more fleshed out and preferably its own class
+// This is just for testing purposes
+
+const codec = new Codec();
+var ws;
+
+function connect () {
+    // Assuming the location is localhost
+    ws = new WebSocket("ws://" + window.location.hostname + ":4000/");
+
+    ws.binaryType = "arraybuffer";
+
+    ws.onopen = () => {
+        ws.send(codec.encode({
+            type: 0,
+            options: {
+                name: "test",
+            }
+        }))
+    }
+}
+
+connect();
