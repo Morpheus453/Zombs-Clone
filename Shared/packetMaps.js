0c6ff16e0d87c1996225bd2f64a50c411ffafe65
Morpheus453 <53342625+Morpheus453@users.noreply.github.com>
2022-09-20 22:19:13 -0400

    Update packetMaps.js

--- Shared/packetMaps.js
+++ Shared/packetMaps.js
@@ -1,7 +1,9 @@
-export const packetMaps = {
+const packetMaps = {
     "REQUEST_JOIN": 0,
     "JOIN_RESPONSE": 1,
 
     0: "REQUEST_JOIN",
     1: "JOIN_RESPONSE",
 }
+
+module.exports = packetMaps;
