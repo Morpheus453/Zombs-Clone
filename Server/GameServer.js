08db7c1ac6b04434c059671025be685a6a560935
Morpheus453 <53342625+Morpheus453@users.noreply.github.com>
2022-09-21 10:05:14 -0400

    Update GameServer.js

--- Server/GameServer.js
+++ Server/GameServer.js
@@ -26,11 +26,26 @@ class GameServer {
 
         this.world.update(dt);
 
+        this.sendVisibleEntities();
+
         this.lastUpdated = now;
     }
 
     addClient (socket) {
         this.clients.push(new Client(this, socket));
+    }
+
+    sendVisibleEntities () {
+        for (let i = 0; i < this.clients.length; i++) {
+            // Checking is set to false for performance
+
+            this.clients[i].send({
+                type: 2,
+                options: {
+                    entities: this.world.getInSight(this.clients[i].id),
+                }
+            });
+        }
     }
 }
 
