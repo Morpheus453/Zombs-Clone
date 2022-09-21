784197075cd38578ca2a8d59873baa28a6b6675a
Morpheus453 <53342625+Morpheus453@users.noreply.github.com>
2022-09-21 10:04:49 -0400

    Update script.js

--- Client/script.js
+++ Client/script.js
@@ -1,11 +1,14 @@
 import { Renderer } from "./renderer.js";
 import Codec from "../Shared/codec.js";
+import packetMaps from "../Shared/packetMaps.js";
+import packets from "../Shared/packets";
 
 // In the future this will be more fleshed out and preferably its own class
 // This is just for testing purposes
 
 const codec = new Codec();
 var ws;
+var myId;
 
 function connect () {
     // Assuming the location is localhost
@@ -20,6 +23,22 @@ function connect () {
                 name: "test",
             }
         }))
+    }
+
+    ws.onmessage = m => {
+        let msg = codec.decode(m.data);
+
+        console.log(msg);
+
+        switch (packetMaps[msg.type]) {
+            case "JOIN_RESPONSE":
+                myId = msg.options.id;
+                console.log(myId);
+                break;
+            case "WORLD_DATA":
+                console.log(msg.options.entities);
+                break;
+        }
     }
 }
 
